37104cb54d87c2827d9477b64d1f80d71587863f
Javier Arellano <franjarca@hotmail.com>
2016-10-17 13:35:53 +0200

    subir ej1 hasta aptdo c ws21

--- Worksheet21/ej1.js
+++ Worksheet21/ej1.js
@@ -1,19 +1,46 @@
 /*
-1. Define una función máximo que dados 4 valores devuelva el máximo de ellos.
-Llámala con un ejemplo pidiendo los 4 valores al usuario.
+Write examples with Arrays to solve the next problems using only Array methods (like iterators, etc...):
+
+a) find largest number
+
+b) find longest string
+
+c) find even numbers (pares)
+
+d) find odd numbers (impares)
+
+e) find words that contain 'is'
+
+f) assert all numbers are divisible by three
+
+g)  zip two arrays together
+
+h) sort joined array from smallest to largest
+
+i) remove the first word in the array
+
+j) place a new word at the start of the array
+
+k) replace some elements
+
+l) Over an array with names, find all entries whose firstname starts with 'J',  create projection combined of only the initials of the name and then sort alphabetically
 */
 
 
 function maximo(lis){
     return lis.sort()[lis.length-1];
 }
-
+function largo(lis){
+    return lis.sort(function(a, b){return a.length-b.length})[lis.length-1];
+}
+/*a
 window.onload = function(){
-    var num1 = parseInt(prompt('introduce un numero'));
-    var num2 = parseInt(prompt('introduce un numero'));
-    var num3 = parseInt(prompt('introduce un numero'));
-    var num4 = parseInt(prompt('introduce un numero'));
-    var arra = [num1, num2, num3, num4];
+    var arra = [4,35,234,5,123,3,4,5,6,72,234,2];
     var ma = maximo(arra);
     document.getElementById('pio').innerHTML = ma;
+}*/
+window.onload = function(){
+    var arra = ['asdf','qwerasgf','asd','qwergasbasdf'];
+    var larg = largo(arra);
+    document.getElementById('pio').innerHTML = larg;
 }
